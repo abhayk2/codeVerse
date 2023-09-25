15d5cbde45970e523f854313133cd70abd94406b
Abhay kumar <abhaykumar@Abhays-MacBook-Air.local>
2023-09-26 01:27:16 +0530

    changes in useTitle

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,9 @@
 import { useRef } from "react";
+import { useTitle } from "../hooks/useTitle";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { login } from "../services";
-import { useTitle } from "../hooks/useTitle";
+
 
 export const Login = () => {
     useTitle("Login-CodeVerse");
